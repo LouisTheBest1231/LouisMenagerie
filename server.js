169301288dcfed96536f5db12f0e676b491b3685
LouisTheBest1231 <53878474+LouisTheBest1231@users.noreply.github.com>
2023-10-05 22:20:23 -0400

    test build

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const httpServer = http.createServer(app)
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'build','index.html'));
+});
+
+const port = process.env.PORT || 3000;
+httpServer.listen(port, ()=>{
+    console.log('SERVER : Starting...');
+    console.log('SERVER : Listening on port : ' + port);
+});
