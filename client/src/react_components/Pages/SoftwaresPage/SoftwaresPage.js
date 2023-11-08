a6894eb53b82b47bd454bd57227d2afeeb6fabcb
Louis-Etienne Messier <53878474+LouisTheBest1231@users.noreply.github.com>
2023-11-07 23:25:47 -0500

    Software Layout Test

--- client/src/react_components/Pages/SoftwaresPage/SoftwaresPage.js
+++ client/src/react_components/Pages/SoftwaresPage/SoftwaresPage.js
@@ -1,15 +1,40 @@
 
+import ProjectModule from "../../ProjectModule/ProjectModule";
 import "./SoftwaresPage.css"
 
 export default function SoftwaresPages(){
     return (
         <div className="softwarespage">
             <div className="softwarespage_title">{">> "}SOFTWARES</div>
-            <div className="softwarespage_subtitle">A little taste of the softwares I developped</div>
+
+            <div className="softwarespage_content">
+                
+                <div className="softwarespage_subtitle">
+                    <div>
+                        <div>A little taste of the softwares I developped</div>
+                        <br/>
+                        <div> Click on them for more informations!</div>
+                    </div>
+                    
+                </div>
             
-            <div className="softwarespage_modules"
+                <div className="softwarespage_modules">
+                    {GetModuleList(20).map(function(o){
+                        return o
+                    })}
+                </div>
             
-            ></div>
+            </div>
+            
         </div>
     );
+}
+
+//FOR TEST ONLY
+function GetModuleList(number){
+    const list = []
+    for(let i =0; i < number ;i++){
+        list.push(<ProjectModule number={i}/>);
+    }
+    return list;
 }
---
+++ client/src/react_components/ProjectModule/ProjectModule.js
@@ -0,0 +1,9 @@
+
+import "./ProjectModule.css"
+export default function ProjectModule({number}){
+    return (
+        <div className="projectmodule">
+            MODULE {number}
+        </div>
+    );
+}
