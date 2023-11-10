e5fe191c14b68759550f4666d827d22640329ec9
Louis-Etienne Messier <53878474+LouisTheBest1231@users.noreply.github.com>
2023-11-09 23:28:29 -0500

    LAST_GREENCONSOLE_COMMIT

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import ContentBox from './react_components/ContentBox/ContentBox';
 import Navbar from './react_components/Navbar/Navbar';
+import "./index.css"
 
 function App() {
   return (
--- client/src/react_components/Navbar/NavbarContent/NavbarContent.js
+++ client/src/react_components/Navbar/NavbarContent/NavbarContent.js
@@ -4,23 +4,40 @@ import "./NavbarContent.css"
 import logo from "./Logo-100X100.png"
 import NavbarButtons from "./NavbarButtons/NavbarButtons";
 
+import {IoIosMore} from "react-icons/io";
+import { IconContext } from "react-icons";
+
 export default function NavbarContent(){
     return (
+        <div className="navbar_content_parent">
         <div className="navbar_content">
            
            <Link to="/">
                 <div className="navbar_element">   
                     <img src={logo} alt="LM Logo"></img>
-                    <div>LOUIS' MENAGERIE</div>
+                    <div className="navbar_element_LM">LOUIS' MENAGERIE</div>
                 </div>
            </Link>
              
-            <CustomLink to="/softwares" text="Softwares"/>
-            <CustomLink to="/games" text="Games"/>
-            <CustomLink to="/webpages" text="Webpages"/>
-            <CustomLink to="/donate" text="Donate"/>
-            <CustomLink to="/about" text="About"/>
+            <div className="navbar_element_softwares">
+                <CustomLink to="/softwares" text="Softwares"/></div>
+
+            <div className="navbar_element_games">
+                <CustomLink to="/games" text="Games"/></div>
+            <div><CustomLink to="/webpages" text="Webpages"/></div>
+            <div><CustomLink to="/donate" text="Donate"/></div>
+            <div><CustomLink to="/about" text="About"/></div>
             
+            
+
+        </div>
+        <div className="navbar_hamburger">
+            <IconContext.Provider value={{className:"navbar_hamburger_logo"}}>
+            <IoIosMore/>
+            </IconContext.Provider>
+
+        </div>
+
         </div>
     );
 }
