5d5ac7d3847dd018399649f83426339e1597b333
Louis-Etienne Messier <53878474+LouisTheBest1231@users.noreply.github.com>
2023-11-16 00:31:26 -0500

    reset page

--- client/src/react_components/Page/Page.js
+++ client/src/react_components/Page/Page.js
@@ -9,7 +9,6 @@ export default function Page() {
             <div className="page_content">
 
                 <AboutPage/>
-                <DonatePage/>
             </div>
             <Footer />
         </div>
