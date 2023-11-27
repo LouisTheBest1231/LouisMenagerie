dbc25543f7c6f10a58f975d78c7b17b59da0fab7
Louis-Etienne Messier <53878474+LouisTheBest1231@users.noreply.github.com>
2023-11-26 22:52:45 -0500

    Adding the Dynamic DropDown menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
 
+import {useState } from "react";
 import "./index.css"
 import IndexModule from "./react_components/IndexModule/IndexModule";
 import Navbar from "./react_components/Navbar/Navbar";
 import Page from "./react_components/Page/Page";
+import useScreenShort from "./react_components/HelperModules/useScreenShort";
 
 function App() {
+
+
+  const [isDropDown, setIsDropDown] = useState(false)
+
+
+  function toggleDropDown(){
+    setIsDropDown(!isDropDown)
+  }
+
   return (
     <div className="app">
       
 
           
-      {<Navbar />}
-      {<IndexModule/>}
-       
-  
-     
-        
-      <div className="not_fixed">
-        <Page/>
+      {<Navbar toggleDropDown={toggleDropDown}setIsDropDown={setIsDropDown} isDropDown={isDropDown}/>}
+      {<IndexModule setIsDropDown={setIsDropDown} isDropDown={isDropDown} isScreenShort={useScreenShort()}/>}
+      <div className={useScreenShort() ? "not_fixed_short" : "not_fixed"}> 
+        {!isDropDown &&<Page/>} 
       </div>
 
 
