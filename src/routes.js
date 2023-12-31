47543bc5a857db54caf459229d98c7400828a17f
LouisTheBest1231 <53878474+LouisTheBest1231@users.noreply.github.com>
2023-12-31 00:41:06 -0500

    Change of the server file to create the sitemap

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+const routes = [
+    {path: '/', exact: true, component: 'AboutPage'},
+    {path: '/donate', component: 'DonatePage'},
+
+    {path: '/notasandbox', component:'Page'},
+    {path: '/blackjackcoop', component:'Page'},
+    {path: '/programmer101', component:'Page'},
+    {path: '/thegame', component:'Page'},
+    {path: '/apagos', component:'Page'},
+    {path: '/testingdirectx11', component:'Page'},
+    {path: '/nba2k', component:'Page'},
+    {path: '/rpg', component:'Page'},
+    {path: '/td', component:'Page'},
+    {path: '/consolesnake', component:'Page'},
+    {path: '/consolecalculator', component:'Page'},
+    {path: '/consolehangman', component:'Page'},
+];
+
+module.exports = routes;
